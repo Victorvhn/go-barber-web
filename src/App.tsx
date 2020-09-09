f5a4ec041ecd641ef2ea782bbc6a1f9413cfbf43
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-09-09 20:59:49 -0300

    Estrutura de toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import SignIn from './pages/SignIn';
+import ToastContainer from './components/ToastContainer';
 
 import { AuthProvider } from './hooks/AuthContext';
 
@@ -12,6 +13,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
   </>
 );
 
