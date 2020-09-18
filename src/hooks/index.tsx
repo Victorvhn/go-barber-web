d687a0eee4dfb92a277e2bf4e0646ed38ede369d
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-09-18 01:45:07 -0300

    Adicionando e removendo toast

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { AuthProvider } from './auth';
 import { ToastProvider } from './toast';
-import ToastContainer from '../components/ToastContainer';
 
 const AppProvider: React.FC = ({ children }) => (
   <AuthProvider>
     <ToastProvider>{children}</ToastProvider>
-    <ToastContainer />
   </AuthProvider>
 );
 
