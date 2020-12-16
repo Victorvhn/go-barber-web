0248eff30524fe44d514d879a6495502cd537c29
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-16 20:00:03 -0300

    Enviando formulário a API

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,13 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Button as CustomButtom } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isLoading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, isLoading, ...rest }) => (
   <CustomButtom type="button" {...rest}>
-    {children}
+    {isLoading ? 'Carregando...' : children}
   </CustomButtom>
 );
 
--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { FiLogIn, FiMail } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -13,12 +13,14 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, AnimationContainer, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import api from '../../services/api';
 
 interface ForgotPasswordFormData {
   email: string;
 }
 
 const ForgotPassword: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const formRef = useRef<FormHandles>(null);
 
   const { addToast } = useToast();
@@ -26,6 +28,7 @@ const ForgotPassword: React.FC = () => {
   const handleSubmit = useCallback(
     async (data: ForgotPasswordFormData) => {
       try {
+        setIsLoading(true);
         formRef.current?.setErrors({});
 
         const schema = Yup.object().shape({
@@ -35,6 +38,17 @@ const ForgotPassword: React.FC = () => {
         });
 
         await schema.validate(data, { abortEarly: false });
+
+        await api.post('/password/forgot', {
+          email: data.email,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'E-mail de recuperação enviado',
+          description:
+            'Enviamos uma e-mail para confirmar a recuperação de senha, cheque sua caixa de entrada',
+        });
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
           const errors = getValidationErrors(error);
@@ -50,6 +64,8 @@ const ForgotPassword: React.FC = () => {
           description:
             'Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente.',
         });
+      } finally {
+        setIsLoading(false);
       }
     },
     [addToast],
@@ -68,7 +84,9 @@ const ForgotPassword: React.FC = () => {
               type="text"
               placeholder="E-mail"
             />
-            <Button type="submit">Recuperar</Button>
+            <Button isLoading={isLoading} type="submit">
+              Recuperar
+            </Button>
           </Form>
           <Link to="/">
             <FiLogIn />
